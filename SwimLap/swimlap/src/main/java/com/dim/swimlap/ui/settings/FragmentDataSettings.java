7451e71403a84135486422570c32bf0cbae8c662
stephenbroyerproject <stephen.broyer@gmail.com>
2014-04-15 10:53:37 +0200

    UI Settings done. And change logo

--- SwimLap/swimlap/src/main/java/com/dim/swimlap/ui/settings/FragmentDataSettings.java
+++ SwimLap/swimlap/src/main/java/com/dim/swimlap/ui/settings/FragmentDataSettings.java
@@ -12,15 +12,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.dim.swimlap.R;
 
 public class FragmentDataSettings extends Fragment {
 
+    private DatePicker datePickerStart, datePickerStop;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_data_settings, container, false);
+        datePickerStart = (DatePicker) view.findViewById(R.id.id_datepicker_startdate);
+        datePickerStop = (DatePicker) view.findViewById(R.id.id_datepicker_stopdate);
+
         return view;
     }
 }
